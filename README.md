6974d093965767dddf98abdbed1bcb33f86e7094
Mubasher <mobi_20_11@yahoo.com>
2023-04-04 23:40:23 +0200

    Basic Header Layout Is Ready

--- README.md
+++
@@ -1,1 +0,0 @@
-# Food-order-App
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import HeaderImage from "../assets/Header-image.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton/>
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={HeaderImage} alt="A Table Of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
