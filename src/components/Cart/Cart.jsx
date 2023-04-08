46ff21b238f6a500764c28c47dfcd9dec626f438
Mubasher <mobi_20_11@yahoo.com>
2023-04-08 02:27:27 +0200

    Project functionality is done except Css am on it

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,33 @@
-import React,{useContext} from "react";
-import classes from "./Cart.module.css";
+import React, { useContext } from "react";
+import classes from './Cart.module.css'
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import CartContext from "../../components/store/cart-context";
 
 function Cart(props) {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-  const cartCtx= useContext(CartContext)
-  const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`
-  const hasItems=cartCtx.items.length >0;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const CartItemAddHandler = (item) => {
+    cartCtx.addItem({...item,amount:1})
+  };
 
   const cartItems = (
-    <ul className={classes["cart-item"]}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name} </li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={CartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
